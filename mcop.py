8ce8002604214360563a47167aa6cef5be13d0ce
Sejan Dhakal <dhakalsejan124@gmail.com>
2024-12-26 19:15:58 -0600

    update

--- mcop.py
+++ mcop.py
@@ -1,14 +1,15 @@
 import datetime as dt 
+import yfinance as yf
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
-import yfinance as yf
+
 
 # Getting the necessary data
 def getData(stocks, start, end):
-    stockData = yf.download(stocks, start, end)
-    stockData = stockData['Close']
-    returns = stockData.pct_change()
+    stocksData = yf.download(stocks, start, end)
+    stocksData = stocksData['Close']
+    returns = stocksData.pct_change()
     meanReturns = returns.mean()
     covarianceMatrix = returns.cov()
     return meanReturns, covarianceMatrix
@@ -18,6 +19,9 @@ stocks = [stock + '.AX' for stock in stockList]
 endDate = dt.datetime.now()
 startDate = endDate - dt.timedelta(days=300)
 meanReturns, covarianceMatrix = getData(stocks, startDate, endDate)
-print(meanReturns)
+
+weights = np.random.random(len(meanReturns)) 
+weights /= np.sum(weights)
+print(weights)
 
 
